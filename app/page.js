3c8f22d302f475953188bf5941a5aac20b8ca509
jmtorralvo <jm.torralvo@one-beyond.com>
2024-12-06 13:44:00 +0100

    feat(): add db

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import classes from './page.module.css';
-import ImageSlideshow from '@/components/image-slideshow';
+import Link from "next/link";
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
@@ -25,8 +25,8 @@ export default function Home() {
           <h2>How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
@@ -38,8 +38,8 @@ export default function Home() {
           <h2>Why NextLevel Food?</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
---
+++ components/main-header/main-header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import NavLink from "@/components/atoms/nav-link";
+import MainHeaderBackground from "./main-header-background";
+import classes from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            alt="food recipes task"
+            width={100}
+            height={100}
+            priority
+          />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
